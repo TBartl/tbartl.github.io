9d2fb7f98827c630bd62a0d29efbbb78bc54c935
TBartl <tbartl@umich.edu>
2017-09-20 16:49:32 -0400

    Cleaned up Infinite Space

--- script.js
+++ script.js
@@ -84,19 +84,16 @@ var layers = document.getElementsByClassName("layer");
 var screenRatio = 1;
 
 function setWindowSize() {
+    console.log('Hey');    
     var myWidth;
-    // var myHeight;
     if (typeof (window.innerWidth) == 'number') {
         myWidth = window.innerWidth;
-        // myHeight = window.innerHeight;
     } else {
         if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
             myWidth = document.documentElement.clientWidth;
-            // myHeight = document.documentElement.clientHeight;
         } else {
             if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
                 myWidth = document.body.clientWidth;
-                // myHeight = document.body.clientHeight;
             }
         }
     }
